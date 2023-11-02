bb7c4344344bc646aa5f3fb720f8f91e32e672ce
Oleksandr Kulkov <hutofsato@gmail.com>
2023-11-02 23:20:26 +0200

    Added some style changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,22 @@ const teasCollection = defineCollection({
     })
 });
 
+const postsCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      author: z.string().optional(),
+      description: z.string(),
+      pubDate: z.date(),
+      image: z.string().optional(),
+      featured: z.boolean().optional(),
+      draft: z.boolean().optional(),
+      category: z.array(z.string()).optional(),
+      tags: z.array(z.string())
+    })
+})
+
 export const collections = {
   teas: teasCollection,
+  library: postsCollection,
 };

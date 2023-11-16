3c2d5fb5610ef4861179ffe68c84b7fcbf1453bc
Oleksii Yakuba <oleksii.y@mavericks.agency>
2023-11-16 18:23:05 +0200

    re-path

--- astro.config.cjs
+++ astro.config.cjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import swup from '@swup/astro';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), swup({
-    theme: 'fade',
-    reloadScripts: true
-  }), mdx()]
+	integrations: [
+		tailwind(),
+		swup({
+			theme: 'fade',
+			reloadScripts: true,
+		}),
+		mdx(),
+	],
 });
